a01a184182fe5d00e8212bdbbaca911dedc065a2
Hunter Callaway <HunterCallaway@hotmail.com>
2023-01-10 00:04:06 -0600

    Created an initial route to test the API.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+# We set the safe parameter to False to receive any Python data type.
+def get_routes(request):
+  return JsonResponse('Our API', safe=False)
