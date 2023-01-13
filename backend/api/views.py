3c4d60c78d39754613ecc3a1e03a08ef97852a41
Hunter Callaway <HunterCallaway@hotmail.com>
2023-01-12 22:56:04 -0600

    Started working with the Django REST Framework.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -8,4 +8,48 @@ This view will return a list of objects that contain information about our route
 We set the safe parameter to False to receive any Python data type.
 '''
 def get_routes(request):
-  return JsonResponse('Our API', safe=False)
+  routes = [
+        {
+            'Endpoint': '/trips/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of trips.'
+        },
+        {
+            'Endpoint': '/trips/id/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single trip object.'
+        },
+        {
+            'Endpoint': '/trips/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates a new trip with data sent in post request.'
+        },
+        {
+            'Endpoint': '/trips/id/item/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates a new item with data sent in post request.'
+        },
+        {
+            'Endpoint': '/trips/id/item/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Updates an existing itm with data sent in put request.'
+        },
+        {
+            'Endpoint': '/trips/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes an exiting trip.'
+        },
+        {
+            'Endpoint': '/trips/id/item/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes an exiting item.'
+        },
+    ]
+  return Response(routes)
