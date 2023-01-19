451e27d372cd862934ff833f95a43f7df9c29d49
Hunter Callaway <HunterCallaway@hotmail.com>
2023-01-18 20:17:54 -0600

    Initial Header component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      Packing CheckList App
+      <Header />
+      Trips
     </div>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+      Packing Checklist App
+      </h1>
+    </div>
+  )
+}
+
+export default Header
