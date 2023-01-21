7ccd6f6c8870b48f5c6d92ffbfed587ee8be13af
Hunter Callaway <HunterCallaway@hotmail.com>
2023-01-20 21:33:08 -0600

    Worked on the getTrips() function.

--- frontend/src/pages/TripsListPage.js
+++ frontend/src/pages/TripsListPage.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+/*
+DRY Notes:
+1. Since there won't initially be any data,
+   the initial state of our trips should be an empty array.
+2a. If we don't use the `await` operator in the `getTrips` function,
+    we will receive a Promise that isn't the data itself.
+2b. We need to wait until the previous `fetch()` method has finished
+    before we parse the body text as JSON.
+*/
 
 const TripsListPage = () => {
+
+  let [trips, setTrips] = useState([]);
+
+  useEffect(() => {
+    getTrips();
+  }, []);
+
+  //Returns all Trip instances.
+  let getTrips = async () => {
+    let response = await fetch('http://127.0.0.1:8000/api/trips/');
+    let data = await response.json();
+    console.log('Data: ', data);
+    setTrips(data);
+  };
+
   return (
     <div>
       Your Upcoming Trips
