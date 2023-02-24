029411dc62b145931025c26ee0ce7978fe87f08e
Hunter Callaway <HunterCallaway@hotmail.com>
2023-02-23 21:32:19 -0600

    Added the update-trip path.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
   path('', views.get_routes, name="routes"),
   
   path('trips/', views.get_trips, name="trips"),
+  path('trips/<str:pk>/update/', views.update_trip, name="update-trip"),
 
   path('trips/<str:pk>/', views.get_trip, name="trip"),
 ]
